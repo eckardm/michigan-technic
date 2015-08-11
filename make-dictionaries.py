e0e884553739926a82f8e2557487bbcc979ffa85
eckardm <eckardm@umich.edu>
2015-08-11 17:05:06 -0400

    initial plan for making dictionaries

---
+++ make-dictionaries.py
@@ -0,0 +1,12 @@
+'''
+first things first, import the python modules we'll need, listed in the order you'll need them'''
+
+
+'''
+go through each of the volumes in a collection, make dictionary entries for title, date, id'''
+
+'''
+tokenize ocr and make dictionary entry'''
+
+'''
+write dictionaries to file for reference later'''
