730c1a02fa688f341fbf1926d8e9b2e7fc85f033
eckardm <eckardm@umich.edu>
2015-08-12 08:47:12 -0400

    import what we need

--- make-dictionaries.py
+++ make-dictionaries.py
@@ -1,6 +1,16 @@
 '''
 first things first, import the python modules we'll need, listed in the order you'll need them'''
 
+# urllib2 opens URLs, it comes with python
+import urllib2
+
+# beautifulsoup pulls data out of html and xml files, you'll need to install it
+from bs4 import BeautifulSoup
+
+# nltk is a leading platform for building python programs to work with human language data, you'll need to install it
+import nltk
+from nltk import word_tokenize
+
 
 '''
 go through each of the volumes in a collection, make dictionary entries for title, date, id'''
