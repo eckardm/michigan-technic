d261e347edc803f1c8014ce43604c65487b16fec
eckardm <eckardm@umich.edu>
2015-08-12 12:01:38 -0400

    completes dictionary making

--- make-dictionaries.py
+++ make-dictionaries.py
@@ -12,7 +12,6 @@ import re
 
 # nltk is a leading platform for building python programs to work with human language data, you'll need to install it
 import nltk
-from nltk import word_tokenize
 
 
 '''
@@ -40,41 +39,68 @@ def make_dictionaries(collection_url):
     make a way to get dictionary entries for row results and row result alts'''
     
     # function to get dictionary entries
-    def get_row_results(div_class_attribute_value)
+    def get_row_results(div_class_attribute_value):
     
         # finding all of the results
         row_results = collection_soup.find_all('div', {'class': div_class_attribute_value})
         
+        # make an emply list we'll use later
+        dictionaries = []
+        
         # go through row results
         for row_result in row_results:
-            # find the title
-            row_result_title = row_result.find('h4', {'class': 'Title'})
-            # get just what we need
-            row_result_title = re.sub('Item\s\d+\:\s', '', row_result_title.text)
+            # find the title, remove articles from beginning
+            title = re.sub('Item\s\d+\:\s', '', row_result.find('h4', {'class': 'Title'}).text).lstrip('The ').lstrip('An ').lstrip('A ')
             # find the date
-            row_result_date = row_result.find('span', {'class': 'Date'})
-            # get just what we need
-            row_result_date = int(row_result_date.text.replace('Published ', ''))
-            # find the id
-            row_result_id = row_result.find('input', {'class': 'id'})
-            # get just what we need
-            row_result_id = row_result_id['value']
-            
+            date = int(row_result.find('span', {'class': 'Date'}).text.replace('Published ', ''))
+            # find the id, replaces the period because python doesn't like periods in variable names
+            id = row_result.find('input', {'class': 'id'})['value']
+           
             
             '''
             tokenize the ocr for row results'''
+            
+            # open ocr
+            ocr = open('ocr/' + id + '.txt', 'r')
+            # read and decode data
+            ocr_data = ocr.read().decode('utf-8')
+            # tokenize
+            ocr_tokens = nltk.word_tokenize(ocr_data)
+            # close the ocr
+            ocr.close()
             
             
             '''
             make dictionary for row result'''
             
-            # make a dictionary
-            
+            # replaces the period in the id because python doesn't like periods in variable names
+            dictionary_name = id.replace('.', '_')
+            # initialize dictionary
+            id = {}
+            # add the title
+            id['title'] = title
+            # add the date
+            id['date'] = date
+            # add the id
+            id['id'] = row_result.find('input', {'class': 'id'})['value']
+            # add the tokens
+            id['ocr'] = ocr_tokens
+                        
             # append the dictionary to our list
+            dictionaries.append(id)
             
             
             '''
             write dictionaries to file for reference later'''
+            
+            # go through each dictionary in the list
+            for dictionary in dictionaries:
+                # opening file so that we can append to it
+                with open('dictionaries.py', 'a') as dictionaries_file:
+                    # writing the dictionaries
+                    dictionaries_file.write(dictionary_name + ' = ' + str(id))
+                    # and skipping a space
+                    dictionaries_file.write('\n')
         
         
     '''
