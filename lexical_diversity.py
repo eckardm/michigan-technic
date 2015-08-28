5b8b021833274fb4a1851b7de6b6dd0d1ffb1d05
Max Eckard <eckardm@umich.edu>
2015-08-28 09:56:02 -0400

    gets total words

--- lexical_diversity.py
+++ lexical_diversity.py
@@ -5,11 +5,23 @@ import what we need'''
 import os
 from os.path import join
 
+# nltk is a leading platform for building python programs to work with human language data, you'll need to install it
+import nltk
+
 
 '''
 get total words'''
 
-
+# go through each ocr file
+for filename in os.listdir('ocr'):
+    # open it in super read mode
+    with open(join('ocr', filename), 'rb') as ocr:
+        # read it
+        raw = ocr.read().decode('utf-8')
+        # tokenize it
+        text = nltk.word_tokenize(raw)
+        # print the filename and unique words
+        print filename + ': ' + str(len(text))
 
 
 '''
