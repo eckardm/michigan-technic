b45df00527573022a780a17902636fb309d0c074
Max Eckard <eckardm@umich.edu>
2015-08-28 09:40:31 -0400

    it's a hyphen not a dash

--- lexical_diversity.py
+++ lexical_diversity.py
@@ -1,6 +1,18 @@
 '''
 import what we need'''
 
+# os provides a portable way of using operating system dependent functionality
+import os
+from os.path import join
+
+# import the dictionaries we created earlier
+# go through the ocr directory and get the filename
+for filename in os.listdir('ocr'):
+    # create the dictionary name
+    dictionary = filename.replace('.', '_')
+    # and import the dictionary
+    from dictionaries import dictionary
+    
 
 '''
 get total words'''
