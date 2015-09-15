7640bd91cadcfe07143dc121acf634e1261cde1f
Max Eckard <eckardm@umich.edu>
2015-09-15 10:05:31 -0400

    updating comments

--- word-occurences-in-context.py
+++ word-occurences-in-context.py
@@ -8,25 +8,21 @@ from os.path import join
 # nltk is a leading platform for building python programs to work with human language data, you'll need to install it
 import nltk
 
-# matplotlib is a python 2d plotting library which produces publication quality figures in a variety of hardcopy formats and interactive environments across platforms, you'll need to install it
-import matplotlib.pyplot as plt
-
 from dictionaries import dictionary_sho
 
 '''
 preliminaries'''
 
 # print what we're doing
-print 'FREQUENT WORD COMBINATIONS'
+print 'WORD OCCURENCES IN CONTEXT'
 print '=========================='
 print '\n'
 
 # words to look for
-words_to_look_for = ['Albert Kahn', 'concrete']
-
+words_to_look_for = ['Albert', 'Kahn', 'concrete']
 
 '''
-get total words'''
+get word occurences in context'''
 
 # go through each ocr file
 for filename in os.listdir('ocr'):
