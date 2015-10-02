6f3aa74396bd8384484a161e42595c77616bce77
Max Eckard <eckardm@umich.edu>
2015-10-02 08:54:33 -0400

    commenting

--- ocr-total.py
+++ ocr-total.py
@@ -1,12 +1,30 @@
+'''
+first things first, import what we need'''
+
 import os
 from os.path import join
+
+
+'''
+preliminaries'''
 
 # where is the folder of ocr files?
 ocr_folder = 'ocr'
 
-# go through each file in that folder
+
+'''
+get ocr for entire run'''
+
+# go through each file in the ocr folder
 for ocr_file in os.listdir(ocr_folder):
+    # open the ocr file
     ocr = open(join(ocr_folder, ocr_file), 'r')
+    # go through each line in the ocr file
     for line in ocr:
+        # open the ocr total file
         with open('ocr-total.txt', 'ab') as ocr_total:
+            # write it to the ocr total file
             ocr_total.write(line)
+    # close the ocr file
+    ocr.close()
+            
