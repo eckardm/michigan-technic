4a00e63a73a26566fff26d7fc03c936a80b61aaf
Max Eckard <eckardm@umich.edu>
2015-10-02 11:38:25 -0400

    tqdm again

--- ocr-total.py
+++ ocr-total.py
@@ -1,8 +1,12 @@
 '''
 first things first, import what we need'''
 
+# os provides a portable way of using operating system dependent functionality
 import os
 from os.path import join
+
+# adds a progress meter to your loops in a second
+from tqdm import *
 
 
 '''
@@ -16,11 +20,12 @@ ocr_folder = 'ocr'
 get ocr for entire run'''
 
 # go through each file in the ocr folder
-for ocr_file in os.listdir(ocr_folder):
+for ocr_file in tqdm(os.listdir(ocr_folder)):
+    print '\n'
     # open the ocr file
     ocr = open(join(ocr_folder, ocr_file), 'r')
     # go through each line in the ocr file
-    for line in ocr:
+    for line in tqdm(list(ocr)):
         # open the ocr total file
         with open('ocr-total.txt', 'ab') as ocr_total:
             # write it to the ocr total file
